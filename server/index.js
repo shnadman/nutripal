8ffe04a094d324f4931a9df3d3e22113157efcc2
Nadav Sherf <nsherf@gmail.com>
2021-02-14 21:02:14 +0200

    More fixes

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")();
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
